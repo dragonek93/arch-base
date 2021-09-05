03e3a68b208dafca5504f50de2cd7c9037db7a86
dragonek93 <64033605+dragonek93@users.noreply.github.com>
2021-09-05 03:33:14 +0200

    arch-base
    basic arch linux install script of services and bootloader and basic config

---
+++ arch-base.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+hwclock --systohc --utc
+sed -i '177s/.//' /etc/locale.gen
+sed -i '390s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=pl_PL.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=pl" >> /etc/vconsole.conf
+echo "FONT=Lat2-Terminus16.psfu.gz" >> /etc/vconsole.conf
+echo "FONT_MAP=8859-2" >> /etc/vconsole.conf
+echo "dragon-attacK" >> /etc/hostname
+echo "127.0.0.1         localhost" >> /etc/hosts
+echo "::1               localhost" >> /etc/hosts
+echo "127.0.1.1         dragon-attacK.localdomain   dragon-attacK" >> /etc/hosts
+echo root:password | chpasswd
+useradd -m -g users -G wheel -s /bin/bash dragonek
+echo dragonek:password | chpasswd
+sed -i '82/.//' /etc/sudoers
+
+#SERVICES
+pacman -S base-devel linux-headers networkmanager openssh firewalld reflector tlp
+systemctl enable NetworkManager
+systemctl enable sshd
+systemctl enable firewalld
+systemctl enable reflector.timer
+systemctl enable tlp
+
+#BOOTLOADER
+pacman -S grub efibootmgr os-prober dosfstools intel-ucode
+grub-install --target=x86_64-efi --bootloader-id=ARCH_LINUX --efi-directory=/boot/EFI --recheck
